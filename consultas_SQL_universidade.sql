469e95aad1966bc558fee1d371b43bdb6bab3282
Eduardo Cadilhe <eduardo.00.1du@gmail.com>
2023-08-30 13:27:56 -0300

    Consultas das questões 1 a 4 responiddas

--- consultas_SQL_universidade.sql
+++ consultas_SQL_universidade.sql
@@ -1,24 +1,72 @@
--- Obter o os nomes dos professores que são do departamento denominado'Informática', sejam doutores,
+-- 1. Obter o os nomes dos professores que são do departamento denominado 'Informática', sejam doutores,
 -- e que, em 932 (2osemestre de 1993),ministraram alguma turma de disciplina do departamento 'Informática' 
 -- que tenha mais que três créditos.
 
-SELECT `professor`.`codProf`,
-    `professor`.`nomeProf`,
-    `professor`.`codTit`,
-    `professor`.`codDepto`
-FROM `universidade`.`professor`
-INNER JOIN profturma ON professor.codProf=profturma.codProf and anoSem='932'
-INNER JOIN titulacao
-ON professor.codTit=titulacao.codTit and nomeTit='Doutor'
-INNER JOIN depto
-ON depto.codDepto=professor.codDepto and nomeDepto='informatica'
-INNER JOIN new_view  NV
-ON NV.numDisc=profturma.numDisc and NV.siglaTur=profturma.siglaTur
-GROUP BY nomeProf;
+CREATE VIEW profturmadisc AS
+SELECT profTurma.*
+FROM profturma,disciplina
+WHERE profturma.codDepto=(
+	SELECT depto.codDepto 
+    FROM depto 
+    WHERE depto.nomeDepto='informatica'
+    )
+and disciplina.codDepto=(
+	SELECT depto.codDepto 
+    FROM depto 
+    WHERE depto.nomeDepto='informatica'
+    )
+and profTurma.anoSem='932'
+and disciplina.creditosDisc>3
+and disciplina.numDisc=profturma.numDisc;
+
+SELECT distinct `professor`.`nomeProf`
+FROM `universidade`.`professor`,profturmadisc
+WHERE codTit=(
+	SELECT codTit 
+    FROM titulacao 
+    WHERE nomeTit='doutor') 
+and professor.codDepto=(
+	SELECT depto.codDepto 
+    FROM depto 
+    WHERE depto.nomeDepto='informatica'
+    )
+and profturmadisc.codProf=professor.codProf;
 
 -- 2. Obter o número de disciplinas do departamento de ‘Informática’.
 
-SELECT Disc.codDepto,Disc.numDisc,Disc.nomeDisc,Disc.creditosDisc FROM universidade.disciplina as Disc JOIN universidade.depto DPT ON  Disc.codDepto=DPT.codDepto and DPT.nomeDepto='informatica';
+SELECT count(numDisc) as 'Numeros de Disciplina'
+FROM `universidade`.`disciplina`
+WHERE disciplina.codDepto=(
+	SELECT depto.codDepto 
+    FROM depto 
+    WHERE depto.nomeDepto='informatica'
+    );
 
--- Obter o número de salas que foram usadas no ano-semestre 941 (1osemestre de 1994) 
+
+-- 3. Obter o número de salas que foram usadas no ano-semestre 941 (1osemestre de 1994) 
 -- por turmas do departamento de 'Informática'.
+
+SELECT numDisc,siglaTur,count(numSala)
+FROM horario 
+WHERE codDepto=(
+	SELECT codDepto 
+    FROM depto 
+    WHERE nomeDepto='informatica'
+    )
+and horario.anoSem='941' 
+GROUP BY siglaTur,numDisc
+ORDER BY numDisc;
+
+-- 4. Obter os nomes das disciplinas do departamento denominado 'Informática' que têm o maior número
+-- de créditos dentre as disciplinas deste departamento.
+SELECT disciplina.nomeDisc
+FROM disciplina
+WHERE disciplina.codDepto=(
+	SELECT codDepto 
+    FROM depto 
+    WHERE nomeDepto='informatica'
+    )
+and creditosDisc=(
+	SELECT max(creditosDisc)
+    FROM disciplina
+    );
