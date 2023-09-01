db42629f8c1333a9c316d0902385c43e71ff03f4
Eduardo Nunes Cadilhe <ecadilhe@acad.ifma.edu.br>
2023-08-31 21:09:27 -0300

    Consultas Questao 1 a 14. Utilizando database universidade

--- consultas_SQL_universidade.sql
+++ consultas_SQL_universidade.sql
@@ -114,12 +114,9 @@ WHERE EXISTS(SELECT disciplina.codDepto
 -- 9. Obter os identificadores das salas (código do prédio e número da sala) que,
 -- em 1994/1: nas segundas-feiras (dia da semana = 2), tiveram ao menos uma turma
 -- do departamento 'Informática'.
-SELECT sala.codPred,sala.numSala
-FROM sala
-INNER JOIN horario
-ON horario.numSala=sala.numSala 
-and horario.codPred=sala.codPred
-and horario.codDepto=(
+SELECT horario.codPred,horario.numSala
+FROM horario
+WHERE horario.codDepto=(
 	SELECT depto.codDepto 
 	FROM depto 
 	WHERE depto.nomeDepto='informatica'
@@ -128,13 +125,54 @@ and horario.diaSem='2'
 and horario.anoSem='941';
     
 -- 10. Mostre a ocupação média das salas em cada prédio no primeiro semestre de 1994.
+SELECT P.nomePred, AVG(T.capacidade), AVG(S.capacidade)
+FROM Turma T 
+	JOIN horario H 
+    ON T.anoSem = H.anoSem 
+	and T.codDepto = H.codDepto 
+	and T.numDisc = H.numDisc 
+    and T.siglaTur = H.siglaTur
+JOIN sala S 
+	ON H.codPred = S.codPred 
+	and H.numSala = S.numSala 
+JOIN predio P 
+    ON S.codPred = P.codPred
+WHERE T.anoSem = 941 
+GROUP BY P.nomePred;
 
 -- 11. Para cada prédio, liste a sala que tem a maior capacidade.
+SELECT nomePred, numSala, sala.capacidade 
+FROM sala, predio
+WHERE sala.capacidade = (
+    SELECT max(sala.capacidade) 
+    FROM sala s2
+    WHERE s2.codPred = sala.codPred)
+GROUP BY nomePred;
 
 -- 12. Qual é a sala mais utilizada? Liste o número da sala e quantas vezes ela foi usada.
+SELECT H.numsala, COUNT(*) as quantUso, H.codPred 
+FROM Horario H
+GROUP BY H.numsala 
+ORDER BY quantUso DESC limit 1;
 
 -- 13. Liste o número da sala e quantas vezes ela foi usada (ordem decrescente).
+SELECT H.numsala, COUNT(*) as quantUso
+FROM Horario H 
+GROUP BY H.numsala 
+ORDER BY quantUso desc;
 
 -- 14. Crie duas visões para as seguintes consultas:
--- ● Obter o código da sala e o código do prédio, desde que a salatenha capacidade superior a 35 lugares.
+-- ● Obter o código da sala e o código do prédio, desde que a sala tenha capacidade superior a 35 lugares.
 -- ● Sabendo que cada crédito de disciplina corresponde a 15 hora/aula,retorne o nome da disciplina e o seu número de horas-aula.
+
+CREATE VIEW salaMaior35 as 
+	SELECT numSala, codPred
+    FROM Sala 
+    WHERE sala.capacidade > 35;
+
+CREATE VIEW disiciplinaHorasAula as
+	SELECT nomedisc, creditosdisc * 15
+    FROM Disciplina;
+    
+SELECT * FROM salaMaior35;
+SELECT * FROM disiciplinaHorasAula;
